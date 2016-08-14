00b486a0d3b2597cdf6e92a0f65ed294f7d9762b
binson1989 <binson1989@gmail.com>
2016-08-15 01:38:54 +0530

    Add message in validations

--- AspNetWebApiStaThreading/ValidateModelStateAttribute.cs
+++ AspNetWebApiStaThreading/ValidateModelStateAttribute.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FluentValidation;
+using FluentValidation.Results;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -6,6 +8,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http.Controllers;
+using System.Web.Http.Dependencies;
 using System.Web.Http.Filters;
 
 namespace AspNetWebApiStaThreading
@@ -19,6 +22,67 @@ namespace AspNetWebApiStaThreading
                 actionContext.Response = actionContext.Request.CreateErrorResponse(
        HttpStatusCode.BadRequest, actionContext.ModelState);
             }
+
+            //base.OnActionExecuting(actionContext);
+            //var requestScope = actionContext.Request.GetDependencyScope();
+
+
+            //var parameters = actionContext.ActionArguments.Select(x => x.Value).Where(x => x != null);
+
+            //foreach (var parameter in parameters)
+            //{
+            //    var argumentType = parameter.GetType();
+            //    var validator = FindValidator(argumentType);
+            //    if (validator != null)
+            //    {
+            //        var validationResult = validator.Validate(parameter);
+            //        if (!validationResult.IsValid)
+            //        {
+            //            ThrowFormattedApiResponse(validationResult);
+            //        }
+            //    }
+            //}
         }
+
+        //private IValidator FindValidator(Type type, IDependencyScope depScope)
+        //{
+
+        //    // Can be replaced with custom IOC logic
+        //    if (type == typeof(Company))
+        //    {
+        //        depScope.GetService()
+        //    }
+        //    return null;
+        //}
+
+        //private void ThrowFormattedApiResponse(ValidationResult validationResult)
+        //{
+        //    var errorsModel = new ErrorsModel();
+
+        //    var formattedErrors = validationResult.Errors.Select(x =>
+        //    {
+        //        var errorModel = new ErrorModel();
+        //        var errorState = x.CustomState as ErrorState;
+        //        if (errorState != null)
+        //        {
+        //            errorModel.ErrorCode = errorState.ErrorCode;
+        //            errorModel.Field = x.PropertyName;
+        //            errorModel.Documentation = "https://developer.example.com/docs" + errorState.DocumentationPath;
+        //            errorModel.DeveloperMessage = string.Format(errorState.DeveloperMessageTemplate, x.PropertyName);
+
+        //            // Can be replaced by translating a localization key instead
+        //            // of just mapping over a hardcoded message
+        //            errorModel.UserMessage = errorState.UserMessage;
+        //        }
+        //        return errorModel;
+        //    });
+        //    errorsModel.Errors = formattedErrors;
+
+        //    var responseMessage = new HttpResponseMessage(HttpStatusCode.BadRequest)
+        //    {
+        //        Content = new StringContent(JsonConvert.SerializeObject(errorsModel, Formatting.Indented))
+        //    };
+        //    throw new HttpResponseException(responseMessage);
+        //}
     }
 }
--- AspNetWebApiStaThreading/Validators.cs
+++ AspNetWebApiStaThreading/Validators.cs
@@ -7,11 +7,16 @@ using System.Threading.Tasks;
 
 namespace AspNetWebApiStaThreading
 {
+    /// <summary>
+    /// Error code can be directly used as message in fluent validation
+    /// can add description attribute to enum, and description can be given string values.
+    /// as mentioned in sergey's page, in validation action filter we can return our custom object having validation errors.
+    /// </summary>
     public class CompanyValidator : AbstractValidator<Company>
     {
         public CompanyValidator(IValidator<Address> addressValidator)
         {
-            RuleFor(x => x.Name).NotEmpty();
+            RuleFor(x => x.Name).NotEmpty().WithMessage("E003");
             RuleFor(x => x.Address).SetValidator(addressValidator);
         }
     }
@@ -20,8 +25,8 @@ namespace AspNetWebApiStaThreading
     {
         public AddressValidator()
         {
-            RuleFor(x => x.City).NotEmpty();
-            RuleFor(x => x.State).NotEmpty();
+            RuleFor(x => x.City).NotEmpty().WithMessage("Name is empty....fix it!!!");
+            RuleFor(x => x.State).NotEmpty().WithMessage("Name is empty....fix it!!!");
         }
     }
 }
